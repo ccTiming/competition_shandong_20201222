31703dd09e8a8bd786e185177233cf26c8a54a53
WENCHAO Fu <wxf975@cs.bham.ac.uk>
2020-12-25 11:14:40 +0800

    fix

--- seq2seq/attention.py
+++ seq2seq/attention.py
@@ -14,29 +14,32 @@ class Attention(nn.Module):
             self.dense1_cat = nn.Linear(2 * encoder_hidden_size, encoder_hidden_size, bias=False)
             self.dense2_cat = nn.Linear(encoder_hidden_size, 1)
 
-    def forward(self, hidden_state, encoder_output):
+    def forward(self, decoder_hidden_state, encoder_output):
         """
-        :param hidden_state: [num_layers*directional,batch_size,encoder_hidden_size]
+        通常设置decoder_hidden_size和encoder_hidden_size相同
+        :param decoder_hidden_state: [num_layers*directional,batch_size,decoder_hidden_size]
         :param encoder_output: [batch_size,seq_len,encoder_hidden_size]
         :return:
         """
         if self.method == 'dot':
-            hidden_state = hidden_state[-1, :, :]  # [1,batch_size,encoder_hidden_size]
-            hidden_state = hidden_state.permute(1, 2, 0)  # [batch_size,encoder_hidden_size,1]
+            decoder_hidden_state = decoder_hidden_state[-1, :, :]  # [1,batch_size,decoder_hidden_size]
+            decoder_hidden_state = decoder_hidden_state.permute(1, 2, 0)  # [batch_size,decoder_hidden_size,1]
             # attention: [batch_size,seq_len,1]-->[batch_size,seq_len]
-            attention = encoder_output.bmm(hidden_state).squeeze(-1)
+            attention = encoder_output.bmm(decoder_hidden_state).squeeze(-1)
             attention_weight = F.softmax(attention)
         elif self.method == 'general':
             encoder_output = self.dense_general(encoder_output)  # [batch_size,seq_len,encoder_hidden_size]
-            # hidden_state:[1,batch_size,encoder_hidden_size]-->[batch_size,encoder_hidden_size,1]
-            hidden_state = hidden_state[-1, :, :].permute(1, 2, 0)
-            attention = encoder_output.bmm(hidden_state).squeeze(-1)
+            # hidden_state:[1,batch_size,decoder_hidden_size]-->[batch_size,decoder_hidden_size,1]
+            decoder_hidden_state = decoder_hidden_state[-1, :, :].permute(1, 2, 0)
+            attention = encoder_output.bmm(decoder_hidden_state).squeeze(-1)
             attention_weight = F.softmax(attention)  # [batch_size,seq_len]
         elif self.method == 'concat':
             batch_size, seq_len = encoder_output.size()[:2]
-            hidden_state = hidden_state[-1, :, :].squeeze(0)  # [batch_size,encoder_hidden_size]
-            hidden_state.repeat(1, seq_len, 1)  # [batch_size,seq_len,encoder_hidden_size]
-            concat = torch.cat([encoder_output, hidden_state], dim=-1)  # [batch_size,seq_len,2*encoder_hidden_size]
+            decoder_hidden_state = decoder_hidden_state[-1, :, :].squeeze(0)  # [batch_size,decoder_hidden_size]
+            decoder_hidden_state.repeat(1, seq_len, 1)  # [batch_size,seq_len,decoder_hidden_size]
+            concat = torch.cat([encoder_output, decoder_hidden_state],
+                               dim=-1)  # [batch_size,seq_len,decoder_hidden_size+encoder_hidden_size]
+            # attention:[batch_size,seq_len]
             attention = self.dense2_cat(F.tanh(self.dense1_cat(concat.view((batch_size * seq_len, -1))))).squeeze(-1)
             attention_weight = F.softmax(attention.view((batch_size, seq_len)))
         else:
